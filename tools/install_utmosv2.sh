4df44163f845fbc22e2ddc6cf043e48b6e1ee1a2
ftshijt <728307998@qq.com>
2025-04-24 03:08:59 -0400

    fix to install utmosv2

--- tools/install_utmosv2.sh
+++ tools/install_utmosv2.sh
@@ -7,7 +7,7 @@ fi
 # Check if git-lfs is installed
 if git lfs --version >/dev/null 2>&1; then
   echo "Git LFS is installed."
-  git-lfs install
+  git lfs install
 else
   echo "Git LFS is not installed. Please install git-lfs first."
   echo "You may check tools/install_gitlfs.md for guidelines"
@@ -20,4 +20,4 @@ cd UTMOSv2
 GIT_LFS_SKIP_SMUDGE=1 pip install -e .
 cd ..
 
-echo "Be aware to execute `source activate_utmosv2.sh` to enable the checkpoint"
+echo "Be aware to execute 'source activate_utmosv2.sh' to enable the checkpoint"
