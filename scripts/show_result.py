1d424b87678ee796198e77f3bed9a8af0179e1a6
ftshijt <ftshijt@gmail.com>
2025-06-03 18:21:06 -0500

    update configuration setup

--- scripts/show_result.py
+++ scripts/show_result.py
@@ -9,12 +9,7 @@ file = open(args.input, "r", encoding="utf-8")
 
 info = []
 for line in file:
-    line = (
-        line.strip().replace(" '", ' "').replace("':", '":').replace("inf", "Infinity")
-    )
-    line = line.replace("{'", '{"').replace("',", '",')
-    if "sdr" not in line:
-        continue
+    line = line.strip()
     info.append(json.loads(r"{}".format(line)))
 
 keys = info[0].keys()
